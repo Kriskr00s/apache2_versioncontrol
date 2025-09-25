78e032ce4365844002b6552f71bc29f8dc6edb77
Kriskr00s <valtinkristofer@gmail.com>
2025-09-25 15:29:21 +0300

    teine commit

--- git_pull.sh
+++ git_pull.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-cd /var/www/live
+set -e                      # katkesta, kui mõni käsk ebaõnnestub
+cd /var/www/live            # sihtkoha kaust, kus on git clone
+/usr/bin/git reset --hard   # tühjenda lokaalsed muudatused
+/usr/bin/git pull origin master
+#!/bin/bash
+cd /var/www/source
 git pull origin master
--- sync_live_to_backup.sh
+++ sync_live_to_backup.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-cp -r /var/www/live/* /var/www/html/
-cp -r /var/www/html/* /var/www/backup/
+set -e
+
+SRC="/var/www/live"
+HTML="/var/www/html"
+BACKUP="/var/www/backup"
+
+# 1) kopeeri LIVE → HTML (veebilehe kaust)
+sudo rsync -av --delete "$SRC"/ "$HTML"/
+
+# 2) kopeeri HTML → BACKUP (varundus)
+sudo rsync -av --delete "$HTML"/ "$BACKUP"/
